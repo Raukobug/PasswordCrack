54449c14d19bc1fe23639d180dfe29b71d6070a9
Raukobug <rauko1@gmail.com>
2014-10-22 18:54:29 +0200

    Opstart

---
+++ PasswordCrackerCentralizedClient/PasswordCrackerCentralized/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace PasswordCrackerCentralized
+{
+    class Program
+    {
+        static void Main()
+        {
+            Cracking cracker = new Cracking();
+            Parallel.Invoke(cracker.RunCracking);
+        }
+    }
+}
